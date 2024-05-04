00565bd5ca301139cad1f1eef53213ebf5eaad3f
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-05-04 14:54:25 -0300

    feat: launch hero component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeroComponent } from './components';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })

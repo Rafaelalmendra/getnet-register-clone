17175509fa23868061351da6c57314954fae65a8
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-05-04 16:25:49 -0300

    feat: create card offer structure

---
+++ src/app/components/offer-card/offer-card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './offer-card.component.html',
+  styleUrl: './offer-card.component.scss',
+})
+export class OfferCardComponent {
+  @Input() invoiceOver!: string;
+  @Input() title!: string;
+  @Input() rent!: string;
+  @Input() description!: string;
+  @Input() getPay!: string;
+  @Input() specialCondition!: string;
+
+  constructor() {}
+}
--- src/app/components/offers/offers.component.ts
+++ src/app/components/offers/offers.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
+import { OfferCardComponent } from '../offer-card/offer-card.component';
 import { SelectFilterComponent } from '../select-filter/select-filter.component';
 
 @Component({
   selector: 'app-offers',
   standalone: true,
-  imports: [SelectFilterComponent],
+  imports: [OfferCardComponent, SelectFilterComponent],
   templateUrl: './offers.component.html',
   styleUrl: './offers.component.scss',
 })

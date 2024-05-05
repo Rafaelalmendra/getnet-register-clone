cb5f48a648d2d2e485321c6ff84ba19c7e616cbd
Rafael Almendra <rafaelalmendra28@gmail.com>
2024-05-05 15:45:33 -0300

    feat: launch drawer ui

--- src/app/components/drawer/drawer.component.ts
+++ src/app/components/drawer/drawer.component.ts
@@ -1,19 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 import type { Offering } from '../../types';
 
 @Component({
   selector: 'app-drawer',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './drawer.component.html',
   styleUrl: './drawer.component.scss',
 })
 export class DrawerComponent {
-  @Input({
-    required: true,
-  })
-  offer: Offering = {} as Offering;
+  // @Input({
+  //   required: true,
+  // })
+  // offer: Offering = {} as Offering;
+
+  @Input() offer: Offering | null = null;
   @Output() onClose = new EventEmitter<void>();
 
   constructor() {}
